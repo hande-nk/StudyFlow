ab503c4578dcafd28f9e2ccf05cc00c0ac76e59e
hande-nk <handenazkavas@gmail.com>
2023-08-14 21:51:32 +0300

    Initial Commit

---
+++ StudyFlow/StudyFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyFlowApp.swift
+//  StudyFlow
+//
+//  Created by Hande Naz Kavas on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct StudyFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
