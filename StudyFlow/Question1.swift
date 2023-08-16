2aa35413d35685adbc0fe2094afc530e4ee01680
Victoria Buendia <vbuendiaserrano@gmail.com>
2023-08-16 15:11:33 +0200

    Figured out how to edit the shape of the buttons, currently working on adding a background

--- StudyFlow/Question1.swift
+++ StudyFlow/Question1.swift
@@ -12,51 +12,61 @@ struct Question1: View {
 @State private var answer = ""
     var body: some View {
         NavigationStack {
-            VStack{ //Beginning of VStack
-                
-                Text("Please select the subject you would like to build a study plan for.")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
+            ZStack { //Beginning of zstack
+                Color("lightblue")
+                    .ignoresSafeArea()
+                VStack{ //Beginning of VStack
+                    
+                    Text("Please select the subject you would like to build a study plan for.")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    
+                    Button (action: {
+                        score += 1
+                        answer = "Thank you for your response! Please proceed to the next question"
+                    }) {
+                        Text("Science and Math")
+                    }
                     .padding()
-                
-                Button (action: {
-                score += 1
-                answer = "Thank you for your response! Please proceed to the next question"
-                }) {
-                Text("Science and Math")
-                }
-                .padding()
-                .foregroundColor(Color.yellow)
-                .buttonBorderShape(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=shape: ButtonBorderShape@*/.automatic/*@END_MENU_TOKEN@*/)
-                Button (action: {
-                score += 3
-                answer = "Thank you for your response! Please proceed to the next question"
-                }) {
-                Text("Social Studies")
-                }
-                .padding()
-            
-                Button (action: {
-                score += 5
-                answer = "Thank you for your response! Please proceed to the next question"
-                }) {
-                Text("Languages")
-                }
-                .padding()
-            
-                Button (action: {
-                score += 7
-                answer = "Thank you for your response! Please proceed to the next question"
-                }) {
-                Text("Social Studies")
-                }
-                .padding()
-            
-                Text(answer)
-                .padding()
-
-            } //End of VStack
+                    .background(Capsule().foregroundColor(.white))
+                    .border(.black, width: 2)
+                    .cornerRadius(14)
+//                    .background(Capsule()
+//                        .foregroundColor(.black))
+//                    .buttonStyle(.borderedProminent)
+//                        .tint(Color("lightblue"))
+                    Button (action: {
+                        score += 3
+                        answer = "Thank you for your response! Please proceed to the next question"
+                    }) {
+                        Text("Social Studies")
+                    }
+                    .padding()
+                    
+                    Button (action: {
+                        score += 5
+                        answer = "Thank you for your response! Please proceed to the next question"
+                    }) {
+                        Text("Languages")
+                    }
+                    .padding()
+                    
+                    Button (action: {
+                        score += 7
+                        answer = "Thank you for your response! Please proceed to the next question"
+                    }) {
+                        Text("Social Studies")
+                    }
+                    .padding()
+                    
+                    Text(answer)
+                        .padding()
+                    
+                } //End of VStack
+            } //End of ZStack
             
         }
         
